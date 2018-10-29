b24c650203b4fcb2f276f0a79c011632ae561cf0
Matt Doering <mattdoering86@gmail.com>
2018-10-29 21:56:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif  string.downcase
     "HUH?! SPEAK UP, SONNY!"
-  else  string.downcase
+  elsif  string.downcase
     "NO, NOT SINCE 1938!"
   end
 end
